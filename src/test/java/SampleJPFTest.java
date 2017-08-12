1fba2bddd18f5971d83ced66bed2312755a85de5
Dan Smith <upthewaterspout@apache.org>
2017-08-11 21:28:41 -0700

    Simplifying the test

--- src/test/java/SampleJPFTest.java
+++ src/test/java/SampleJPFTest.java
@@ -1,10 +1,13 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.Properties;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 import gov.nasa.jpf.JPF;
 import gov.nasa.jpf.listener.PreciseRaceDetector;
+import gov.nasa.jpf.util.script.ESParser.Exception;
 import gov.nasa.jpf.util.test.TestJPF;
 import gov.nasa.jpf.vm.SystemState;
 import gov.nasa.jpf.vm.Verify;
@@ -14,12 +17,10 @@ import gov.nasa.jpf.vm.Verify;
  */
 public class SampleJPFTest extends TestJPF {
 
-  @Test
+  @Test(expected =  AssertionError.class)
   public void test() throws InterruptedException {
     Mutate i = new Mutate();
-//    if(verifyNoPropertyViolation("jpf.listener=gov.nasa.jpf.listener.PreciseRaceDetector")) {
     if(verifyNoPropertyViolation()) {
-      Verify.incrementCounter(0);
       Runnable badIncrement = () -> {
         i.value++;
       };
@@ -31,31 +32,6 @@ public class SampleJPFTest extends TestJPF {
       t1.join();
       t2.join();
       assertEquals(2, i.value);
-    } else {
-      assertEquals(1, Verify.getCounter(0));
-    }
-  }
-
-  @Test(expected = AssertionError.class)
-  public void testRace() throws InterruptedException {
-    AtomicInteger i = new AtomicInteger();
-    if(verifyNoPropertyViolation()) {
-
-      //An unsafe increment. Two threads could
-      //call i.get before i.set, and i will
-      //end up being 1 instead of the expected 2
-      Runnable badIncrement = () -> {
-        int value = i.get();
-        i.set(value + 1);
-      };
-
-      Thread t1 = new Thread(badIncrement);
-      Thread t2 = new Thread(badIncrement);
-      t1.start();
-      t2.start();
-      t1.join();
-      t2.join();
-      assertEquals(2, i.get());
     }
   }
 
