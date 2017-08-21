997da90e45680ad0d9304ccc9059a0c32d4e160a
Dan Smith <upthewaterspout@apache.org>
2017-08-21 13:34:12 -0700

    Adding apache license

--- src/test/java/SampleJPFTest.java
+++ src/test/java/SampleJPFTest.java
@@ -1,20 +1,22 @@
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-
-import java.util.Properties;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
-import gov.nasa.jpf.JPF;
-import gov.nasa.jpf.listener.PreciseRaceDetector;
-import gov.nasa.jpf.util.script.ESParser.Exception;
-import gov.nasa.jpf.util.test.TestJPF;
-import gov.nasa.jpf.vm.SystemState;
-import gov.nasa.jpf.vm.Verify;
-
 /**
- * Created by dan on 7/3/17.
+ * Copyright 2017 Dan Smith
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+import org.junit.Test;
+
+import gov.nasa.jpf.util.test.TestJPF;
+
 public class SampleJPFTest extends TestJPF {
 
   @Test(expected =  AssertionError.class)
